48f31d3ef9fee79468a6c86662b78941403b71ce
Cameron Golinsky <camerongolinsky@gmail.com>
2020-12-30 15:00:25 -0800

    it is  a start

--- Desktop/NucampFolder/5-NodeJS-Express-MongoDB/node-express/server.js
+++ Desktop/NucampFolder/5-NodeJS-Express-MongoDB/node-express/server.js
@@ -5,9 +5,13 @@ const port =3000;
 
 const app = express();
 
-app.use((req, res){
+app.use((req, res)=> {
     console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
-    res.end(<html><body><h1>'This is an express server'</h1></body></html>)
+    res.end('<html><body><h1>This is an express server</h1></body></html>')
+})
+
+app.listen(port, hostname, () =>{
+    console.log(`Server is running at http://${hostname}:${port}`)
 })
